52229200146244ff9b79408de4f0624a4ac53144
jdavid2019 <joseph2019d@gmail.com>
2021-05-12 02:51:12 -0500

    first commit

---
+++ src/components/Example.vue
@@ -0,0 +1,63 @@
+<template>
+ <form @submit.prevent="submit">
+    <input type=text class=form-control  name=email placeholder=Email v-model="email" />
+     <div class="error" v-if="!$v.email.required">email is required</div>
+       <div class="error" v-if="!$v.email.email">No es email</div>
+    <input type=text class=form-control  name=tel placeholder=Telefono v-model="tel" />
+         <div class="error" v-if="!$v.tel.required">Tel is required</div>
+    <br>
+    <button :disabled='!isComplete' type=submit>Send Invite</button>
+ </form>  
+</template>
+
+<script>
+import { required,email, minLength } from 'vuelidate/lib/validators';
+
+export default {
+  name: 'Example',
+  data(){
+    return {
+      email: '',
+      tel:''
+      }
+      },
+      methods:{
+        submit(){
+          console.log("ok")
+      }
+      },
+     validations: {
+      email: {
+        required,
+        email,
+        minLength: minLength(2)
+      },
+    tel :{
+      required,
+      }
+      },
+      computed: {
+  isComplete () {
+    return this.email && this.tel;
+  }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
